324b052eb3177826b797df90abff45d3781656a2
Dennis Orlando <dennis.orlando2002@gmail.com>
2024-05-07 17:47:05 +0200

    modify parameters to showcase a request with no json params

--- Products/public_server/scripts/get_events.py
+++ Products/public_server/scripts/get_events.py
@@ -3,8 +3,9 @@ import requests
 # Note: this script will be better used in Linux by piping the output to the 'jq' tool,
 # i.e. python3 get_events.py | jq
 
-url = "http://localhost:8080/events"
-params = { 'mode': '', 'addi': [], 'subi': [], 'addb': [], 'subb': []}
+url = "http://localhost:8080/api/v3/events"
+#params = { 'mode': '', 'addi': [], 'subi': [], 'addb': [], 'subb': []}
+params = {}
 
 response = requests.get(url, json=params)
 

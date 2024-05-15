fdff16eb8b182017911074d7b06f7e10d2f05006
Dennis Orlando <dennis.orlando2002@gmail.com>
2024-05-15 17:41:29 +0200

    Changed mongodb ip to point to mongodb docker

--- Products/management_server/src/main.rs
+++ Products/management_server/src/main.rs
@@ -31,7 +31,7 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
 
     //TODO: solve unwrap
-    let client = Client::with_uri_str("mongodb://localhost:27017").await.unwrap();
+    let client = Client::with_uri_str("mongodb://BeeLive:BeeLive@beelive.mongo:27017").await.unwrap();
     let mongodb_events_collection = client.database("events").collection::<Event>("events");
 
  

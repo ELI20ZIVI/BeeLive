9442b1bb4d64883ae9d54bf23e43c7d118117453
pietrocipriani <cipriani.pietro002@gmail.com>
2024-05-27 18:38:30 +0200

    cargo fix

--- Products/public_server/src/event_processor.rs
+++ Products/public_server/src/event_processor.rs
@@ -4,10 +4,9 @@
 
 use actix_web::{HttpResponse, web};
 use actix_web::web::Data;
-use mongodb::bson::Document;
 use mongodb::Collection;
 
-use crate::dao::{self, objects::{Event, PrunedEvent}};
+use crate::dao::{self, objects::Event};
 use crate::dao::objects::Category;
 use crate::EventQueryData;
 
@@ -33,17 +32,17 @@ pub fn check_add_sub(value: &str, filter: &mut Vec<i32>) -> bool {
 /// Per la documentazione riferirsi a dao::query_pruned_events.
 pub async fn get_events(mongodb_event_collection: Data<Collection<Event>>, mongodb_categories_collection: Data<Collection<Category>>, data: web::Query<EventQueryData>) -> HttpResponse {
 
-    let mut modeFilter = "";
-    let mut addbFilter : Vec<i32> = vec![];
-    let mut subbFilter : Vec<i32> = vec![];
-    let mut addiFilter : Vec<i32> = vec![];
-    let mut subiFilter : Vec<i32> = vec![];;
+    let mut mode_filter = "";
+    let mut addb_filter : Vec<i32> = vec![];
+    let mut subb_filter : Vec<i32> = vec![];
+    let mut addi_filter : Vec<i32> = vec![];
+    let mut subi_filter : Vec<i32> = vec![];
 
     // TODO: Prendi le preferenze degli utenti e applicale ai filtri
     if let Some(m) = &data.mode {
         match m.as_str() {
             "overwrite" | "combine" | "ifempty" => {
-                modeFilter = m.as_str();
+                mode_filter = m.as_str();
             }
             _ => {
                 return HttpResponse::BadRequest().body("Invalid mode value.");
@@ -52,27 +51,27 @@ pub async fn get_events(mongodb_event_collection: Data<Collection<Event>>, mongo
     }
 
     if let Some(value) = &data.addb {
-        if !check_add_sub(value, &mut addbFilter) {
+        if !check_add_sub(value, &mut addb_filter) {
             return HttpResponse::BadRequest().body("Invalid addb value.");
         }
     }
     if let Some(value) = &data.subb {
-        if !check_add_sub(value, &mut subbFilter) {
+        if !check_add_sub(value, &mut subb_filter) {
             return HttpResponse::BadRequest().body("Invalid subb value.");
         }
     }
     if let Some(value) = &data.addi {
-        if !check_add_sub(value, &mut addiFilter) {
+        if !check_add_sub(value, &mut addi_filter) {
             return HttpResponse::BadRequest().body("Invalid addi value.");
         }
     }
     if let Some(value) = &data.subi {
-        if !check_add_sub(value, &mut subiFilter) {
+        if !check_add_sub(value, &mut subi_filter) {
             return HttpResponse::BadRequest().body("Invalid subi value.");
         }
     }
 
-    return dao::query_pruned_events(mongodb_event_collection, mongodb_categories_collection, modeFilter, addbFilter, subbFilter, addiFilter, subiFilter).await;
+    dao::query_pruned_events(mongodb_event_collection, mongodb_categories_collection, mode_filter, addb_filter, subb_filter, addi_filter, subi_filter).await
 }
 
 /// Pass-through a dao::query_full_event_single

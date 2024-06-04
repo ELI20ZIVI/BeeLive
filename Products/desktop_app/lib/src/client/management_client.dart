f6effde5f70739826d4bb9c221f6497b2157373a
Dennis Orlando <dennis.orlando2002@gmail.com>
2024-06-04 10:17:34 +0200

    init management client API endpoints

--- Products/desktop_app/lib/src/client/management_client.dart
+++ Products/desktop_app/lib/src/client/management_client.dart
@@ -14,7 +14,9 @@ class ManagementWebServerClient implements Client {
   final String uriPath;
 
   static const _submitEventUriSegment = "insert_new_event";
-  static const _getEventListUriSegment = "events";
+  static const _getEventListUriSegment = "list_events";
+  static const _deleteEventUriSegment = "delete_event";
+  static const _modifyEventUriSegment = "modify_event";
 
   ManagementWebServerClient(this.uriPath);
 
@@ -22,15 +24,17 @@ class ManagementWebServerClient implements Client {
   Future<http.Response> submitNewEvent(Event event) async {
 
     debugPrint(json.encode(event.toJson()));
-    var uri = Uri.http(uriPath, _submitEventUriSegment);
-    
-    return await http.post(uri, headers: {"Content-Type": "application/json"}, body: json.encode(event.toJson()));
+    var uri = Uri.parse(uriPath + _submitEventUriSegment);
+
+    return await http.post(uri, headers: {"Content-Type": "application/json", "Authorization": "Bearer ${getAuthToken()}"}, body: json.encode(event.toJson()));
   }
 
   @override
-  Future<http.Response> deleteExistingEvent(int eventId) {
-    // TODO: implement deleteExistingEvent
-    throw UnimplementedError();
+  Future<http.Response> deleteExistingEvent(int eventId) async {
+
+    var uri = Uri.http(uriPath, "$_deleteEventUriSegment/$eventId");
+
+    return await http.delete(uri, headers: {"Authorization": "Bearer ${getAuthToken()}"});
   }
 
   @override
@@ -38,8 +42,11 @@ class ManagementWebServerClient implements Client {
 
     var uri = Uri.parse(uriPath + _getEventListUriSegment);
 
-    var response = await http.get(uri);
+    var response = await http.get(uri, headers: {"Authorization": "Bearer ${getAuthToken()}",} );
+
     if (response.statusCode == 200) {
+      debugPrint("${response.statusCode}");
+      debugPrint("${response.body}");
       var json = jsonDecode(response.body) as Iterable;
       return (response, List<Event>.from(json.map((e) => {
         Event.fromJson(e)
@@ -50,9 +57,14 @@ class ManagementWebServerClient implements Client {
   }
 
   @override
-  Future<http.Response> modifyExistingEvent(Event event) {
-    // TODO: implement modifyExistingEvent
-    throw UnimplementedError();
+  Future<http.Response> modifyExistingEvent(Event event) async {
+    debugPrint(json.encode(event.toJson()));
+    var uri = Uri.http(uriPath, "$_modifyEventUriSegment/${event.id}");
+    return await http.put(uri, headers: {"Content-Type": "application/json", "Authorization": "Bearer ${getAuthToken()}"}, body: json.encode(event.toJson()));
+  }
+
+  String getAuthToken() {
+    return "asdfasdf";
   }
 
 }

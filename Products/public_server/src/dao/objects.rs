fff0bb1264853de4d954c8a4e65700c6b2cbd5bc
pietrocipriani <cipriani.pietro002@gmail.com>
2024-06-10 11:16:06 +0200

    DTOs coherence

--- Products/public_server/src/dao/objects.rs
+++ Products/public_server/src/dao/objects.rs
@@ -1,10 +1,11 @@
-
-use chrono::serde::ts_seconds_option;
+use std::iter;
 use chrono::{DateTime, Utc};
-use geojson::{FeatureCollection, Geometry, Feature};
-
+use chrono::serde::ts_seconds_option;
+use geojson::{FeatureCollection, Feature};
+use geojson::Value::Point;
 use mongodb::bson::{doc, Document};
 use serde::{Serialize, Deserialize};
+use serde_repr::{Serialize_repr, Deserialize_repr};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Category {
@@ -32,11 +33,19 @@ impl NullableDateTimeRange {
     }
 }
 
+#[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug, Clone)]
+#[repr(u8)]
+pub enum RiskLevel {
+    Info = 0,
+    Warning = 50,
+    Alert = 100, 
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct SubEvent {
     pub title: String,
     pub description: String,
-    pub geometry: FeatureCollection,
+    pub polygons: FeatureCollection,
     pub validity : NullableDateTimeRange,
 }
 
@@ -50,11 +59,11 @@ pub struct PrunedEvent {
     id: i32,
     title: String,
     summary: String,
-    remote_document: Option<String>,
-    validity: NullableDateTimeRange,
+    validity : NullableDateTimeRange,
     visibility: NullableDateTimeRange,
-    categories: Vec<i32>,
+    category_ids: Vec<i32>,
     polygons: FeatureCollection,
+    risk_level: RiskLevel,
 }
 
 impl PrunedEvent {
@@ -67,12 +76,11 @@ impl PrunedEvent {
             "id": 1,
             "title": 1,
             "summary": 1,
-            "remote_document": 1,
             "validity": 1,
             "visibility": 1,
             "categories": 1,
             "polygons": 1,
-
+            "risk_level": 1,
         }
     }
 }
@@ -83,16 +91,17 @@ impl PrunedEvent {
 /// `locked_by` that are not de/serializable, thus are not sent nor received to / from the client. 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Event {
-    id: i32,
-    title: String,
-    summary: String,
-    description: String,
-    remote_document: Option<String>,
-    validity: NullableDateTimeRange,
-    visibility: NullableDateTimeRange,
-    categories: Vec<i32>,
-    polygons: FeatureCollection,
-    subevents: Vec<SubEvent>,
+    pub id: i32,
+    pub title: String,
+    pub summary: String,
+    pub description: String,
+    pub remote_document: Option<String>,
+    pub validity: NullableDateTimeRange,
+    pub visibility: NullableDateTimeRange,
+    pub categories: Vec<i32>,
+    pub polygons: FeatureCollection,
+    pub subevents: Vec<SubEvent>,
+    pub risk_level: RiskLevel,
     #[serde(skip)]
     pub locked_by: Option<i32>,
     #[serde(skip)]
@@ -102,31 +111,44 @@ pub struct Event {
 impl Event {
     pub fn test_event() -> Event {
 
-        let now = Some(Utc::now());
-        let now = NullableDateTimeRange{begin: now, end: now};
+        let now = Utc::now();
 
         Event {
             id: 0,
-            title: "test".to_string(),
+            title: "Test Event".to_string(),
             description: "an amazing description".to_string(),
             remote_document: Some("test remote document . org".to_string()),
             summary: "a long summary".to_string(),
-            validity: now.clone(),
-            visibility: now,
+            validity: NullableDateTimeRange::new(Some(now), Some(now)),
+            visibility: NullableDateTimeRange::new(Some(now), Some(now)),
             locked_by: None,
-            polygons: FeatureCollection {
-                bbox: None,
-                foreign_members: None,
-                features: vec![Feature{
-                    bbox: None,
-                    foreign_members: None,
-                    id: None,
-                    properties: None,
-                    geometry: Some(Geometry::new(geojson::Value::Point(vec![10.0, 6.0])))
-                }],
-            },
+            polygons: FeatureCollection::from_iter(iter::once(Feature::from(Point(vec![10.0, 6.0])))),
             creator_id: 0,
+            risk_level: RiskLevel::Info,
             subevents: vec![],
+            categories: vec![],
+        }
+    }
+}
+
+
+type UserId = String;
+
+// Utente
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct User {
+    pub id: UserId,
+    pub username: String,
+    pub email: String,
+    pub categories: Vec<u32>,
+}
+
+impl User {
+    pub fn test_user() -> User {
+        User {
+            id: "0".to_owned(),
+            username: "test".to_string(),
+            email: "email@email.com".to_string(),
             categories: vec![],
         }
     }
